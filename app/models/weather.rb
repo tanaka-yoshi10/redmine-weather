99aa476dc4dd2d53dab0ad5e2078b0e1c6a48686
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-05-23 21:34:06 +0900

    remove unuse method

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -2,22 +2,6 @@ class Weather < ActiveRecord::Base
   unloadable
   attr_accessible :city, :country
 
-  def weather
-    begin
-      uri = URI.parse('http://api.openweathermap.org/data/2.5/weather?q=%22' + self.city + ',' + self.country + '%22')
-      # json = Net::HTTP.get(uri)
-      # ex)
-      json = '{"coord":{"lon":135.22,"lat":34.7},"sys":{"message":0.0103,"country":"Japan","sunrise":1429992852,"sunset":1430041179},"weather":[{"id":800,"main":"Clear","description":"Sky is Clear","icon":"01n"}],"base":"stations","main":{"temp":281.198,"temp_min":281.198,"temp_max":281.198,"pressure":1018.27,"sea_level":1034.35,"grnd_level":1018.27,"humidity":84},"wind":{"speed":1.17,"deg":159.5},"clouds":{"all":0},"dt":1430082145,"id":0,"name":"Kobe-Shi","cod":200}'
-
-      JSON.parse(json)["weather"][0]
-    rescue Exception => e
-      logger.warn e
-      logger.warn json
-
-      {"main"=>"error", "icon"=>"01n"}
-    end
-  end
-
   def forecast(count)
     begin
       uri = URI.parse('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + self.city + '&cnt=' + count.to_s)
