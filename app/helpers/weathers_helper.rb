f616d1afb7de6e88448c1ef44b87adc089486883
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-05-23 15:05:38 +0900

    handle exception

--- app/helpers/weathers_helper.rb
+++ app/helpers/weathers_helper.rb
@@ -1,5 +1,9 @@
 module WeathersHelper
   def icontag(forecast) 
-    raw '<td><img src="http://openweathermap.org/img/w/' + forecast["weather"][0]["icon"] + '.png"></td>'
+    unless forecast.nil?
+      raw '<td><img src="http://openweathermap.org/img/w/' + forecast["weather"][0]["icon"] + '.png"></td>'
+    else 
+      raw '<td>error</td>'
+    end
   end
 end
